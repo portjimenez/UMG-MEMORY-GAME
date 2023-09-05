7950c30ed6345d368e67a15bb854634bc396d053
BryanMirand4 <144084103+BryanMirand4@users.noreply.github.com>
2023-09-04 22:09:06 -0600

    Add files via upload

---
+++ memorygame/memorygame/src/Main/Main.java
@@ -0,0 +1,37 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package Main;
+
+import Frame.Principal;
+
+/**
+ *
+ * @author MSBENAVIDES
+ */
+public class Main {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        Principal start = new Principal();
+        start.setVisible(true);
+    }
+
+    public static void setExtendedState(int MAXIMIZED_BOTH) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public void setVisible(boolean b) {
+        throw new UnsupportedOperationException("Not supported yet."); // Generated from nbfs://nbhost/SystemFileSystem/Templates/Classes/Code/GeneratedMethodBody
+    }
+
+    public static class Main {
+
+        public Main() {
+        }
+    }
+}
